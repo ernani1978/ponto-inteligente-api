798a9132243d1905c8aa552f6e06572825f38629
enasc <enasc@DESKTOP-GVJ9J7U>
2021-11-29 14:44:57 -0300

    Commit de classes entities

---
+++ src/main/java/br/com/ernaninascimento/pontointeligente/api/entities/Funcionario.java
@@ -0,0 +1,105 @@
+package br.com.ernaninascimento.pontointeligente.api.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import br.com.ernaninascimento.pontointeligente.api.enums.PerfilEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+@Table(name="funcionario")
+public class Funcionario implements Serializable {
+
+	private static final long serialVersionUID = 2474737381510183261L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false)
+	private String nome;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Column(name = "senha", nullable = false)
+	private String senha;
+	
+	@Column(name = "cpf", nullable = false)
+	private String cpf;
+	
+	@Column(name = "valor_hora", nullable = true)
+	private BigDecimal valorHora;
+	
+	@Transient
+	public Optional<BigDecimal> getValorHoraOpt() {
+		return Optional.ofNullable(valorHora);
+	}
+	
+	@Column(name = "qtd_horas_trabalho_dia", nullable = true)
+	private Float qtdHorasTrabalhoDia;
+	
+	@Transient
+	public Optional<Float> getHorasTrabalhoDiaOpt() {
+		return Optional.ofNullable(qtdHorasTrabalhoDia);
+	}	
+
+	@Column(name = "qtd_horas_almoco", nullable = true)
+	private Float qtdHorasAlmoco;
+	
+	@Transient
+	public Optional<Float> getHorasAlmocoOpt() {
+		return Optional.ofNullable(qtdHorasAlmoco);
+	}	
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "perfil", nullable = false)
+	private PerfilEnum perfil;
+	
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+	
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Empresa empresa;
+	
+	@OneToMany(mappedBy = "funcionario", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Lancamento> lancamentos;
+	
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
+
+}
---
+++ src/main/java/br/com/ernaninascimento/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package br.com.ernaninascimento.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLSE_USUARIO
+}
